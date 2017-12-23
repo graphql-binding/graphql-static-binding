c0b9215e09d2b4eca8ea4880661c37f61696243b
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-12-23 04:43:15 +0100

    New javascript generator for new graphcool-binding

--- src/generators/javascript.ts
+++ src/generators/javascript.ts
@@ -26,70 +26,8 @@ export const generator: Generator = {
 }
 
 function renderHeader(schema: string): string {
-  return `const { FragmentReplacements } = require('graphcool-binding/dist/src/extractFragmentReplacements');
-const { GraphcoolLink } = require('graphcool-binding/dist/src/GraphcoolLink');
-const { buildFragmentInfo, buildTypeLevelInfo } = require('graphcool-binding/dist/src/prepareInfo');
-const { GraphQLResolveInfo, GraphQLSchema } = require('graphql');
-const { GraphQLClient } = require('graphql-request');
-const { SchemaCache } = require('graphql-schema-cache');
-const { delegateToSchema } = require('graphql-tools');
-const { sign } = require('jsonwebtoken');
-
-// -------------------
-// This should be in graphcool-binding
-const schemaCache = new SchemaCache()
-
-class BaseBinding {
-  constructor({
-    typeDefs,
-    endpoint,
-    secret,
-    fragmentReplacements}) {
-    
-    fragmentReplacements = fragmentReplacements || {}
-
-    const token = sign({}, secret)
-    const link = new GraphcoolLink(endpoint, token)
-
-    this.remoteSchema = schemaCache.makeExecutableSchema({
-      link,
-      typeDefs,
-      key: endpoint,
-    })
-
-    this.fragmentReplacements = fragmentReplacements
-
-    this.graphqlClient = new GraphQLClient(endpoint, {
-      headers: { Authorization: \`Bearer \${token}\` },
-    })
-  }
-
-  delegate(operation, prop, args, info) {
-    if (!info) {
-      info = buildTypeLevelInfo(prop, this.remoteSchema, operation)
-    } else if (typeof info === 'string') {
-      info = buildFragmentInfo(prop, this.remoteSchema, operation, info)
-    }
-
-    return delegateToSchema(
-      this.remoteSchema,
-      this.fragmentReplacements,
-      operation,
-      prop,
-      args || {},
-      {},
-      info,
-    )
-  }
-
-  async request(
-    query,
-    variables
-  ) {
-    return this.graphqlClient.request(query, variables)
-  }
-}
-// -------------------
+  return `const { Graphcool, BaseGraphcoolOptions } = require('graphcool-binding')
+const { GraphQLResolveInfo } = require('graphql')
 
 const typeDefs = \`
 ${schema}\``
@@ -97,10 +35,10 @@ ${schema}\``
 
 
 function renderMainMethod(queryType: GraphQLObjectType, mutationType?: GraphQLObjectType | null, subscriptionType?: GraphQLObjectType | null) {
-  return `module.exports.Binding = class Binding extends BaseBinding {
+  return `module.exports.Binding = class Binding extends Graphcool {
   
-  constructor({ endpoint, secret, fragmentReplacements}) {
-    super({ typeDefs, endpoint, secret, fragmentReplacements});
+  constructor({ endpoint, secret, fragmentReplacements, debug }) {
+    super({ typeDefs, endpoint, secret, fragmentReplacements, debug });
 
     var self = this
     this.query = {
@@ -112,8 +50,8 @@ ${renderMainMethodFields('mutation', mutationType.getFields())}
     }`: ''}
   }
   
-  delegate(operation, field, args, info) {
-    return super.delegate(operation, field, args, info)
+  delegate(operation, field, args, context, info) {
+    return super.delegate(operation, field, args, context, info)
   }
 }`
 }
@@ -122,7 +60,7 @@ function renderMainMethodFields(operation: string, fields: GraphQLFieldMap<any, 
   return Object.keys(fields).map(f => {
     const field = fields[f]
     return `      ${field.name}(args, info) { 
-        return self.delegate('${operation}', '${field.name}', args, info)
+        return self.delegate('${operation}', '${field.name}', args, {}, info)
       }`
   }).join(',\n')
 }
