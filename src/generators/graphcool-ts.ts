a2e3ff9dbd08ff0bad2c9b26dfc969add1d84f76
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-12-29 23:34:06 +0100

    style(graphcool-ts): formatting

--- src/generators/graphcool-ts.ts
+++ src/generators/graphcool-ts.ts
@@ -1,21 +1,20 @@
-import { 
-    GraphQLUnionType,
-    GraphQLWrappingType,
-    GraphQLObjectType,
-    GraphQLInputObjectType,
-    GraphQLInputField,
-    GraphQLField,
-    GraphQLInputType,
-    GraphQLOutputType,
-    GraphQLScalarType,
-    GraphQLNamedType,
-
-    isNonNullType,
-    isListType,
-    GraphQLFieldMap,
-    GraphQLEnumType,
-    GraphQLType,
-    GraphQLInterfaceType,
+import {
+  GraphQLUnionType,
+  GraphQLWrappingType,
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLInputField,
+  GraphQLField,
+  GraphQLInputType,
+  GraphQLOutputType,
+  GraphQLScalarType,
+  GraphQLNamedType,
+  isNonNullType,
+  isListType,
+  GraphQLFieldMap,
+  GraphQLEnumType,
+  GraphQLType,
+  GraphQLInterfaceType
 } from 'graphql'
 
 import { Generator } from '../types'
@@ -30,7 +29,7 @@ export const generator: Generator = {
   RootType: renderRootType,
   SchemaType: renderSchemaInterface,
   Main: renderMainMethod,
-  Header: renderHeader,
+  Header: renderHeader
 }
 
 const scalarMapping = {
@@ -49,7 +48,11 @@ const typeDefs = \`
 ${schema}\``
 }
 
-function renderMainMethod(queryType: GraphQLObjectType, mutationType?: GraphQLObjectType | null, subscriptionType?: GraphQLObjectType | null) {
+function renderMainMethod(
+  queryType: GraphQLObjectType,
+  mutationType?: GraphQLObjectType | null,
+  subscriptionType?: GraphQLObjectType | null
+) {
   return `export class Graphcool extends BaseGraphcool {
   
   constructor({ endpoint, secret, fragmentReplacements, debug }: BaseGraphcoolOptions) {
@@ -58,61 +61,96 @@ function renderMainMethod(queryType: GraphQLObjectType, mutationType?: GraphQLOb
   
   query: Query = {
 ${renderMainMethodFields('query', queryType.getFields())}
-  }${mutationType ? `
+  }${
+    mutationType
+      ? `
 
   mutation: Mutation = {
 ${renderMainMethodFields('mutation', mutationType.getFields())}
-  }`: ''}
+  }`
+      : ''
+  }
 }`
 }
 
-
 export function renderMainMethodFields(operation: string, fields: GraphQLFieldMap<any, any>): string {
-  return Object.keys(fields).map(f => {
-    const field = fields[f]
-    return `    ${field.name}: (args, info): Promise<${renderFieldType(field.type)}${!isNonNullType(field.type) ? ' | null' : ''}> => super.delegate('${operation}', '${field.name}', args, {}, info)`
-  }).join(',\n')
+  return Object.keys(fields)
+    .map(f => {
+      const field = fields[f]
+      return `    ${field.name}: (args, info): Promise<${renderFieldType(field.type)}${
+        !isNonNullType(field.type) ? ' | null' : ''
+      }> => super.delegate('${operation}', '${field.name}', args, {}, info)`
+    })
+    .join(',\n')
 }
 
 function renderScalarType(type: GraphQLScalarType): string {
-  if (type.name === 'ID') { return renderIDType(type)}
-  return `${type.description ? `/*
+  if (type.name === 'ID') {
+    return renderIDType(type)
+  }
+  return `${
+    type.description
+      ? `/*
 ${type.description}
 */
-`: ''}export type ${type.name} = ${scalarMapping[type.name] || 'string'}`
+`
+      : ''
+  }export type ${type.name} = ${scalarMapping[type.name] || 'string'}`
 }
 
 function renderIDType(type: GraphQLScalarType): string {
-  return `${type.description ? `/*
+  return `${
+    type.description
+      ? `/*
 ${type.description}
 */
-`: ''}export type ${type.name}_Input = ${scalarMapping[type.name] || 'string'}
+`
+      : ''
+  }export type ${type.name}_Input = ${scalarMapping[type.name] || 'string'}
 export type ${type.name}_Output = string`
 }
 
 function renderEnumType(type: GraphQLEnumType): string {
   return `${renderDescription(type.description)}export type ${type.name} = 
-${type.getValues().map(e => `  '${e.name}'`).join(' |\n')}`
+${type
+    .getValues()
+    .map(e => `  '${e.name}'`)
+    .join(' |\n')}`
 }
-  
+
 function renderRootType(type: GraphQLObjectType): string {
-  const fieldDefinition = Object.keys(type.getFields()).map(f => {
-    const field = type.getFields()[f]
-    return `  ${field.name}: (args: {${field.args.length > 0 ? ' ': ''}${field.args.map(f => `${renderFieldName(f)}: ${renderFieldType(f.type)}`).join(', ')}${field.args.length > 0 ? ' ': ''}}, info?: GraphQLResolveInfo | string) => Promise<${renderFieldType(field.type)}${!isNonNullType(field.type) ? ' | null' : ''}>`
-  }).join('\n')
+  const fieldDefinition = Object.keys(type.getFields())
+    .map(f => {
+      const field = type.getFields()[f]
+      return `  ${field.name}: (args: {${field.args.length > 0 ? ' ' : ''}${field.args
+        .map(f => `${renderFieldName(f)}: ${renderFieldType(f.type)}`)
+        .join(', ')}${
+        field.args.length > 0 ? ' ' : ''
+      }}, info?: GraphQLResolveInfo | string) => Promise<${renderFieldType(field.type)}${
+        !isNonNullType(field.type) ? ' | null' : ''
+      }>`
+    })
+    .join('\n')
 
   return renderTypeWrapper(type.name, type.description, fieldDefinition)
 }
 
 function renderUnionType(type: GraphQLUnionType): string {
-  return `${renderDescription(type.description)}export type ${type.name} = ${type.getTypes().map(t => t.name).join(' | ')}`
+  return `${renderDescription(type.description)}export type ${type.name} = ${type
+    .getTypes()
+    .map(t => t.name)
+    .join(' | ')}`
 }
-  
-function renderObjectType(type: GraphQLObjectType | GraphQLInputObjectType | GraphQLInterfaceType): string {
-  const fieldDefinition = Object.keys(type.getFields()).map(f => {
-    const field = type.getFields()[f]
-    return `  ${renderFieldName(field)}: ${renderFieldType(field.type)}`
-  }).join('\n')
+
+function renderObjectType(
+  type: GraphQLObjectType | GraphQLInputObjectType | GraphQLInterfaceType
+): string {
+  const fieldDefinition = Object.keys(type.getFields())
+    .map(f => {
+      const field = type.getFields()[f]
+      return `  ${renderFieldName(field)}: ${renderFieldType(field.type)}`
+    })
+    .join('\n')
 
   let interfaces: GraphQLInterfaceType[] = []
   if (type instanceof GraphQLObjectType) {
@@ -122,11 +160,15 @@ function renderObjectType(type: GraphQLObjectType | GraphQLInputObjectType | Gra
   return renderInterfaceWrapper(type.name, type.description, interfaces, fieldDefinition)
 }
 
-function renderInputObjectType(type: GraphQLObjectType | GraphQLInputObjectType | GraphQLInterfaceType): string {
-  const fieldDefinition = Object.keys(type.getFields()).map(f => {
-    const field = type.getFields()[f]
-    return `  ${renderFieldName(field)}: ${renderInputFieldType(field.type)}`
-  }).join('\n')
+function renderInputObjectType(
+  type: GraphQLObjectType | GraphQLInputObjectType | GraphQLInterfaceType
+): string {
+  const fieldDefinition = Object.keys(type.getFields())
+    .map(f => {
+      const field = type.getFields()[f]
+      return `  ${renderFieldName(field)}: ${renderInputFieldType(field.type)}`
+    })
+    .join('\n')
 
   let interfaces: GraphQLInterfaceType[] = []
   if (type instanceof GraphQLObjectType) {
@@ -139,37 +181,58 @@ function renderInputObjectType(type: GraphQLObjectType | GraphQLInputObjectType 
 function renderFieldName(field: GraphQLInputField | GraphQLField<any, any>) {
   return `${field.name}${isNonNullType(field.type) ? '' : '?'}`
 }
-  
+
 function renderFieldType(type: GraphQLInputType | GraphQLOutputType) {
   if (isNonNullType(type)) {
     return renderFieldType((type as GraphQLWrappingType).ofType)
-  } 
+  }
   if (isListType(type)) {
     return `Array<${renderFieldType((type as GraphQLWrappingType).ofType)}>`
   }
-  return `${(type as GraphQLNamedType).name}${(type as GraphQLNamedType).name == 'ID' ? '_Output' : ''}`
+  return `${(type as GraphQLNamedType).name}${(type as GraphQLNamedType).name === 'ID' ? '_Output' : ''}`
 }
 
 function renderInputFieldType(type: GraphQLInputType | GraphQLOutputType) {
   if (isNonNullType(type)) {
     return renderInputFieldType((type as GraphQLWrappingType).ofType)
-  } 
-  if (isListType(type)) {
-    return `Array<${renderInputFieldType((type as GraphQLWrappingType).ofType)}> | ${renderInputFieldType((type as GraphQLWrappingType).ofType)}`
   }
-  return `${(type as GraphQLNamedType).name}${(type as GraphQLNamedType).name == 'ID' ? '_Input' : ''}`
+  if (isListType(type)) {
+    return `Array<${renderInputFieldType(
+      (type as GraphQLWrappingType).ofType
+    )}> | ${renderInputFieldType((type as GraphQLWrappingType).ofType)}`
+  }
+  return `${(type as GraphQLNamedType).name}${(type as GraphQLNamedType).name === 'ID' ? '_Input' : ''}`
 }
-  
-function renderSchemaInterface(queryType: GraphQLObjectType, mutationType?: GraphQLObjectType | null, subscriptionType?: GraphQLObjectType | null) {
+
+function renderSchemaInterface(
+  queryType: GraphQLObjectType,
+  mutationType?: GraphQLObjectType | null,
+  subscriptionType?: GraphQLObjectType | null
+) {
   return `export interface Schema {
   query: ${queryType.name}
-${mutationType ? `  mutation: ${mutationType.name}
-` : ''}${subscriptionType ? `  subscription: ${subscriptionType.name}
-` : ''}}`
+${
+    mutationType
+      ? `  mutation: ${mutationType.name}
+`
+      : ''
+  }${
+    subscriptionType
+      ? `  subscription: ${subscriptionType.name}
+`
+      : ''
+  }}`
 }
-  
-function renderInterfaceWrapper(typeName: string, typeDescription: string, interfaces: GraphQLInterfaceType[], fieldDefinition: string): string {
-  return `${renderDescription(typeDescription)}export interface ${typeName}${interfaces.length > 0 ? ` extends ${interfaces.map(i => i.name).join(', ')}`: ''} {
+
+function renderInterfaceWrapper(
+  typeName: string,
+  typeDescription: string,
+  interfaces: GraphQLInterfaceType[],
+  fieldDefinition: string
+): string {
+  return `${renderDescription(typeDescription)}export interface ${typeName}${
+    interfaces.length > 0 ? ` extends ${interfaces.map(i => i.name).join(', ')}` : ''
+  } {
 ${fieldDefinition}
 }`
 }
@@ -181,8 +244,12 @@ ${fieldDefinition}
 }
 
 function renderDescription(description?: string) {
-  return `${description ? `/*
+  return `${
+    description
+      ? `/*
 ${description.split('\n').map(l => ` * ${l}\n`)}
  */
-`: ''}`
+`
+      : ''
+  }`
 }
