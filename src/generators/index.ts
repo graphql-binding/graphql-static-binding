b8f784a57023900e65eef95fd08847f41d90abe5
Kim Brandwijk <kim.brandwijk@gmail.com>
2018-01-15 11:31:19 +0100

    feat(bindings): added prisma bindings (#36)

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -1,5 +1,7 @@
 import { generator as graphcooljsgenerator } from './graphcool-js'
 import { generator as graphcooltsgenerator } from './graphcool-ts'
+import { generator as prismajsgenerator } from './prisma-js'
+import { generator as prismatsgenerator } from './prisma-ts'
 import { generator as bindingtsgenerator } from './binding-ts'
 import { generator as bindingjsgenerator } from './binding-js'
 import { Generator } from '../types';
@@ -7,6 +9,8 @@ import { Generator } from '../types';
 export const generators: { [key: string]: Generator} = {
     'graphcool-js': graphcooljsgenerator,
     'graphcool-ts': graphcooltsgenerator,
+    'prisma-js': prismajsgenerator,
+    'prisma-ts': prismatsgenerator,
     'binding-ts': bindingtsgenerator,
     'binding-js': bindingjsgenerator,
 }
